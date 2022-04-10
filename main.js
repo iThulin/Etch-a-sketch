15c985a936e3b1e83fcd443d2a67140fb6aa02e4
Ian Thulin <ian.thulin@icloud.com>
2022-04-10 15:01:10 -0600

    Create grid container to hold generated pixels

--- main.js
+++ main.js
@@ -4,17 +4,23 @@ const DEFAULT_SIZE = 16;
 
 
 function initiateGrid (size) {
-    const gridContainer = document.getElementById('gridContainer')
-    const pixel = document.createElement('div')
-    
+    var gridContainer = document.getElementById('gridContainer')
 
+    /* Create a grid to hold the pixels. Width and Height are dictated by the 
+    size passed into the function*/
+
+    gridContainer.style.gridTemplateColumns = `repeat(${size}, 1fr)`
+    gridContainer.style.gridTemplateRows = `repeat(${size}, 1fr)`
+
+    /* create a div with the pixel class for each pixel on the drawing board*/
     for (let i = 0; i < size ** 2; i++) {
+        var pixel = document.createElement('div')
         pixel.classList.add("pixel")
         gridContainer.appendChild(pixel);
     }
 }
 
 
-window.onLoad = () => {
+window.onload = () => {
     initiateGrid(DEFAULT_SIZE)
 }
