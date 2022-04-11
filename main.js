cbdd1b02b21c4a034a5fda6d4a5d0889906b8b63
Ian <ian.thulin@icloud.com>
2022-04-11 15:02:59 -0600

    Grid can be adjust in size using the size selector

--- main.js
+++ main.js
@@ -1,11 +1,26 @@
-const DEFAULT_SIZE = 16;
+const DEFAULT_SIZE = 20;
 
+var gridContainer = document.getElementById('gridContainer')
 
+//Clear
+const clearButton = document.querySelector('#clear');
+clearButton.addEventListener('click', clear);
 
+//New Grid
+const newGridButton = document.querySelector("#newGrid");
+newGridButton.addEventListener('click', newGrid);
 
-function initiateGrid (size) {
-    var gridContainer = document.getElementById('gridContainer')
+//Size slider and label
+let sizeSelector = document.querySelector("#gridNumber");
+sizeSelector.value = 20;
+let sizeLabel = document.querySelector("#gridSize")
+sizeLabel.textContent = sizeSelector.value;
+sizeSelector.addEventListener('mousemove', function() {
+    sizeLabel.textContent = sizeSelector.value;
+})
 
+// Functions
+function initiateGrid(size) {
     /* Create a grid to hold the pixels. Width and Height are dictated by the 
     size passed into the function*/
 
@@ -27,6 +42,22 @@ function initiateGrid (size) {
     }
 }
 
+function newGrid() {
+    removePixels();
+    let gridSize = sizeSelector.value;
+    initiateGrid(gridSize);
+    console.log("New Grid button pressed")
+}
+
+function clear() {
+    console.log("clear button pressed")
+}
+
+function removePixels() {
+    while(gridContainer.firstChild) {
+        gridContainer.removeChild(gridContainer.firstChild)
+    }
+}
 
 window.onload = () => {
     initiateGrid(DEFAULT_SIZE)
